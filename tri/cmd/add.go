5691baed189470dc19c0ac8d46413661164e6c36
Dmitry Khrapov <hrapovd@gmail.com>
2021-09-15 15:05:33 +0300

    fix subcomand descriptions

--- tri/cmd/add.go
+++ tri/cmd/add.go
@@ -26,14 +26,9 @@ import (
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: addRun,
+	Short: "Add todo item(s)",
+	Long:  `Add todo item(s) and save into file`,
+	Run:   addRun,
 	//func(cmd *cobra.Command, args []string) {
 	//	fmt.Println("add called")
 	//},
