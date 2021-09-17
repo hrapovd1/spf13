4311b1afe5a1e35e5b4717f5465043e5e6631924
Dmitry Khrapov <hrapovd@gmail.com>
2021-09-17 23:19:08 +0300

    wrote todo tests

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spf13
 CLI App in Go – OSCON 2017
+
+### run tests with lcov coverage files
+#### install extension for VSCodium
+go get -u github.com/jandelgado/gcov2lcov
+#### usage extension
+go test -v -coverprofile=coverage.cover && \
+gcov2lcov -infile=coverage.cover -outfile=coverage.lcov
--- tri/todo/todo_test.go
+++ tri/todo/todo_test.go
@@ -1,6 +1,8 @@
 package todo_test
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 
 	"github.com/hrapovd1/spf13/tri/todo"
@@ -17,3 +19,102 @@ func TestSaveItems(t *testing.T) {
 	err := todo.SaveItems(testFile, items)
 	require.NoError(t, err)
 }
+
+func TestReadItems(t *testing.T) {
+	// create file with json data
+	fileName := "/tmp/read-test-todo.json"
+	items := []todo.Item{
+		{Text: "one", Priority: 1, Done: false},
+		{Text: "two", Priority: 2, Done: true},
+	}
+	b, err := json.Marshal(items)
+	require.Nil(t, err)
+	require.Nil(t, ioutil.WriteFile(fileName, b, 0644))
+
+	// run func ReadItems(filename string) ([]Item, error)
+	_, err = todo.ReadItems(fileName)
+	// check result
+	require.NoError(t, err)
+}
+
+func TestSetPriority(t *testing.T) {
+	priorTests := []struct {
+		pri      int
+		expected int
+	}{
+		{1, 1},
+		{2, 2},
+		{3, 3},
+		{4, 2},
+		{0, 2},
+		{-1, 2},
+	}
+	item := todo.Item{Text: "one", Priority: 1, Done: false}
+
+	for _, pri := range priorTests {
+		item.SetPriority(pri.pri)
+		require.Equal(t, pri.expected, item.Priority)
+	}
+}
+
+func TestPrettyP(t *testing.T) {
+	tests := []struct {
+		pri    int
+		expect string
+	}{
+		{0, " "},
+		{1, "(1)"},
+		{2, "(2)"},
+		{3, "(3)"},
+		{4, " "},
+	}
+	items := []todo.Item{
+		{Text: "text", Priority: 0, Done: false},
+		{Text: "text", Priority: 1, Done: false},
+		{Text: "text", Priority: 2, Done: false},
+		{Text: "text", Priority: 3, Done: false},
+		{Text: "text", Priority: -1, Done: false},
+	}
+	for _, test := range tests {
+		require.Equal(t, test.expect, items[test.pri].PrettyP())
+	}
+}
+
+func TestPrettyDone(t *testing.T) {
+	tests := []struct {
+		done   bool
+		expect string
+	}{
+		{true, "X"},
+		{false, ""},
+	}
+	item := todo.Item{Text: "text", Priority: 0, Done: false}
+	for _, test := range tests {
+		item.Done = test.done
+		require.Equal(t, test.expect, item.PrettyDone())
+	}
+}
+
+/*
+func (i *Item) Label() string {
+	return strconv.Itoa(i.position) + "."
+}
+
+func TestLabel(t *testing.T) {
+	tests := []string{
+		"1.",
+		"2.",
+		"3.",
+	}
+	items := []todo.Item{
+		{Text: "text", Priority: 0, Done: false},
+		{Text: "text", Priority: 1, Done: false},
+		{Text: "text", Priority: 2, Done: false},
+		{Text: "text", Priority: 3, Done: false},
+		{Text: "text", Priority: -1, Done: false},
+	}
+	for n, item := range items {
+		require.Equal(t, tests[n], item.Label())
+	}
+}
+*/
